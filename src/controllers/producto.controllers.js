170ea8459ceac5b936b5e9d6be7a9b30e50771a2
Maria <maria.caram@gmail.com>
2022-07-26 19:25:23 -0300

    se crea la logica para mandar productos a la base de datos de studio 3T

--- src/controllers/producto.controllers.js
+++ src/controllers/producto.controllers.js
@@ -3,6 +3,22 @@ import Producto from "../models/producto";
 const productoCtrl = {};
 
 productoCtrl.borrarProducto = (req, res) => {res.send ("Alguien quiere borrar")}
-productoCtrl.nuevoProducto = (req, res) => {res.send ("Alguien quiere agregar")}
+productoCtrl.nuevoProducto =async(req, res) => {
+    
+    try{
+         const productoNuevo = new Producto({ 
+            nombreProducto: req.body.nombreProducto, 
+            precioProducto: req.body.precioProducto, 
+            foto: req.body.foto, 
+            categoria: req.body.categoria,});
+            
+            console.log(productoNuevo);
+            await productoNuevo.save();
+            res.status(201).json({mensaje:"Producto creado correctamente"})
+        }
+            
+            catch (error)
+            {console.log (error)}
+    ; console.log(req.body)}
 
 export default productoCtrl;
